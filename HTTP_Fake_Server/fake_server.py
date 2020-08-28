2f2dedbf2a68311ba34929de225d2f62797f8290
Torrez, MN <torrez.mn@gmail.com>
2020-08-27 20:59:28 -0400

    Added /basic_profile/<int> regex.

--- HTTP_Fake_Server/fake_server.py
+++ HTTP_Fake_Server/fake_server.py
@@ -3,6 +3,8 @@ from http.server import BaseHTTPRequestHandler
 from urllib import parse
 from faker import Faker
 from random import choice
+import uuid
+import re
 
 # PROVIDERS
 from faker.providers import profile
@@ -10,7 +12,14 @@ from faker.providers import profile
 class Fake_Server(BaseHTTPRequestHandler):
 
     def do_GET(self):
-
+         
+        # ██████╗  █████╗ ███████╗███████╗    ██████╗  ██████╗  ██████╗
+        # ██╔══██╗██╔══██╗██╔════╝██╔════╝    ██╔══██╗██╔═══██╗██╔════╝
+        # ██████╔╝███████║███████╗█████╗      ██║  ██║██║   ██║██║
+        # ██╔══██╗██╔══██║╚════██║██╔══╝      ██║  ██║██║   ██║██║
+        # ██████╔╝██║  ██║███████║███████╗    ██████╔╝╚██████╔╝╚██████╗
+        # ╚═════╝ ╚═╝  ╚═╝╚══════╝╚══════╝    ╚═════╝  ╚═════╝  ╚═════╝
+        
         if self.path == '/':
             self.path = '/public_doc/index.html'
             try:
@@ -22,9 +31,30 @@ class Fake_Server(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(bytes(file_to_open, 'utf-8'))
 
+        
+        # ██████╗ ██████╗  ██████╗ ███████╗██╗██╗     ███████╗
+        # ██╔══██╗██╔══██╗██╔═══██╗██╔════╝██║██║     ██╔════╝
+        # ██████╔╝██████╔╝██║   ██║█████╗  ██║██║     █████╗
+        # ██╔═══╝ ██╔══██╗██║   ██║██╔══╝  ██║██║     ██╔══╝
+        # ██║     ██║  ██║╚██████╔╝██║     ██║███████╗███████╗
+        # ╚═╝     ╚═╝  ╚═╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝╚══════╝
         if self.path == '/basic_profile':
             parsed_path = parse.urlparse(self.path)
             message = json.dumps(str(Faker().simple_profile()))
+            self.send_response(200)
+            self.send_header('Content-Type', 'application/json; charset=utf-8')
+            self.end_headers()
+            self.wfile.write(bytes(message, 'utf-8'))
+        
+        if re.match(r'/basic_profile/\d+', self.path) is not None:
+            
+            total_req = int(self.path.split('/')[-1])
+            req = {
+            'req_url': self.path,
+            'quantity_req': self.path.split('/')[-1],
+            'profiles': [Faker().simple_profile() for i in range(0, total_req)]
+            }
+            message = json.dumps(str(req))
             self.send_response(200)
             self.send_header('Content-Type', 'application/json; charset=utf-8')
             self.end_headers()
@@ -39,7 +69,6 @@ class Fake_Server(BaseHTTPRequestHandler):
             self.send_header('Content-Type', 'application/json; charset=utf-8')
             self.end_headers()
             self.wfile.write(bytes(message, 'utf-8'))
-
         if self.path == '/full_name':
             parsed_path = parse.urlparse(self.path)
             message = json.dumps(str(Faker().name()))
@@ -62,19 +91,53 @@ class Fake_Server(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(bytes(message, 'utf-8'))
 
+        # ██╗   ██╗ █████╗ ██████╗ ██╗███████╗██████╗
+        # ██║   ██║██╔══██╗██╔══██╗██║██╔════╝██╔══██╗
+        # ██║   ██║███████║██████╔╝██║█████╗  ██║  ██║
+        # ╚██╗ ██╔╝██╔══██║██╔══██╗██║██╔══╝  ██║  ██║
+        #  ╚████╔╝ ██║  ██║██║  ██║██║███████╗██████╔╝
+        #   ╚═══╝  ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚══════╝╚═════╝
         if self.path == '/task':
             parsed_path = parse.urlparse(self.path)
             task = {
-                'id':33,
+                'id':str(uuid.uuid4()),
                 'task': Faker().sentence(),
+                'responsible': str(Faker().name()),
                 'status': choice(['completed', 'pending', 'finished', 'lacks_approval']),
-                'created': Faker().date()
+                'created': str(Faker().date()),
+                'priority_level' : choice(['high ',' medium ',' low ',' moderate ',' undefined']),
+                'term_in_days': choice([i for i in range(0,30)])
             }
             message = json.dumps(str(task))
             self.send_response(200)
             self.send_header('Content-Type', 'application/json; charset=utf-8')
             self.end_headers()
             self.wfile.write(bytes(message, 'utf-8'))
+
+        if re.match(r'/task/\d+',self.path) is not None:
+            parsed_path = parse.urlparse(self.path)
+            total_req = int(self.path.split('/')[-1])
+            req = {
+            'req_url':self.path,
+            'quantity_req': self.path.split('/')[-1],
+            'tasks':[{
+                'id':str(uuid.uuid4()),
+                'task': Faker().sentence(),
+                'responsible': str(Faker().name()),
+                'status': choice(['completed', 'pending', 'finished', 'lacks_approval']),
+                'created': str(Faker().date()),
+                'priority_level' : choice(['high ',' medium ',' low ',' moderate ',' undefined']),
+                'term_in_days': choice([i for i in range(0,30)])
+            } for i in range(0,total_req)],
+            }
+            
+                
+            message = json.dumps(str(req))
+            self.send_response(200)
+            self.send_header('Content-Type', 'application/json; charset=utf-8')
+            self.end_headers()
+            self.wfile.write(bytes(message, 'utf-8'))
+
 
 
 
